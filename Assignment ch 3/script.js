4db3dae5bc279b65458f7001dede0613058d9913
MEHAK MEHTAB <mehakmehtab01@gmail.com>
2024-10-26 14:56:18 +0500

    Add files via upload

---
+++ Assignment ch 3/script.js
@@ -0,0 +1,23 @@
+// Assignment : CHAPTER # 3; 
+// Variables For Numbers;
+
+// Question no 01;
+let age = 19 ;
+alert("I am " + age + " years old");
+
+// // Question no 02;
+let visit = prompt("How many times you have visited this site");
+alert("You have visited this site " + visit + " times ");
+
+// Question no 03;
+let birthYear = 2005;
+document.write("My birth year is  " + birthYear+ "<br>");
+document.write( "Data type of my declared variable is " +  typeof(birthYear ));
+
+// Question no 04;
+let visitorName = prompt("Please enter your name");
+let productTitle =  prompt("Please enter your product title");
+let quantity =  prompt("Please enter your product quantity");
+
+document.write(visitorName + " ordered " + quantity  + "   " +   productTitle + " on XYZ Clothing store")
+
