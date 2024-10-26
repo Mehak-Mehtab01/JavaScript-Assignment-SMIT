b418386cfa11f9d2d99d6035a388e0f8c63d0f0e
MEHAK MEHTAB <mehakmehtab01@gmail.com>
2024-10-26 13:37:28 +0500

    Add files via upload

---
+++ Assignment ch 2/script.js
@@ -0,0 +1,35 @@
+// Assignment : CHAPTER # 2; 
+// Variables For String;
+
+// Question no 01;
+let username;
+
+// // Question no 02;
+let myName = "Mehak Mehtab";
+
+// Question no 03;
+let message = alert("Hello World");
+
+// Question no 04;
+
+// Part 1;
+let name = alert("Mehak Mehtab");
+// Part 2;
+let age = alert("15 years old");
+// Part 3;
+let course = alert("Certified Mobile Application Development");
+
+// Question no 05;
+let pizza = alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// Question no 06;
+let email = alert(" My email address is mehakmehtab01@gmail.com");
+
+// Question no 07;
+let book = alert("I am trying to learn from the Book A smarter  \n way to learn JavaScript");
+
+// Question no 08;
+document.write("Yah! I can write HTML content throught JavaScript");
+
+// Question no 09;
+let string = alert("---------৳♟🎇♟৳---------")
