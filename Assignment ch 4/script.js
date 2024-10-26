57763fe26add9670db881edfe4021d9fb58077a4
MEHAK MEHTAB <mehakmehtab01@gmail.com>
2024-10-26 15:21:01 +0500

    Add files via upload

---
+++ Assignment ch 4/script.js
@@ -0,0 +1,34 @@
+// Assignment : CHAPTER # 4; 
+// VARIABLE NAMES: LEGAL & ILLEGAL;
+
+// Question no 01;
+let name , age , email ;
+
+// Question no 02;
+
+// 5 legal variable names;
+let firstName;
+let first_Name;
+let $firstName;
+let firstName1;
+let _fName;
+
+// 5 illegal variable names;
+
+let 1firstName;
+let first-Name;
+ let -firstname;
+ let first Name;
+let @firstName
+
+// Question no 03;
+// Part a ;
+document.write("<h3> Rules for naming JS variables </h3> <br> <br>");
+// Part b ;
+document.write("Variable names can only contain , number ,$ and _ . For example $my_1stVariable <br>");
+// Part c ;
+document.write("Variables must begin with a  letter , $ or _ . For example $name, _name or name <br>");
+// Part d ;
+document.write("Variable names are case sensitive <br>");
+// Part d ;
+document.write("Variable names should not be JS keyword");
